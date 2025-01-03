fc73c8a4ace90b5177205e0c7e5bef774dd6409a
Mohamed Afify <101507500+Mohamed-Mohamed-Afifi@users.noreply.github.com>
2025-01-03 04:47:00 +0200

    Update hero.component.jsx

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div id="hero">
-      <h3>Mohamed Afify</h3>
+      <h3>Mohamed Mohamed Afify</h3>
       <h1>&lt; Full-stack java developer/&gt;</h1>
       <p>
       Highly skilled in building scalable and efficient web applications, leveraging React.js for dynamic, user-friendly front-end interfaces and Spring Boot for robust, back-end microservices. Proficient in designing, developing, and integrating RESTful APIs, ensuring seamless communication between systems. Experienced in implementing modern software development practices, including agile methodologies, test-driven development, and CI/CD pipelines. Passionate about delivering clean, maintainable code and creating innovative solutions to complex technical challenges.
