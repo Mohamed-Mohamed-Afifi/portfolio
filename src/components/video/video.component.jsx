b4aff178cbf6be16b3ed9c45fbffe4e717853161
Mohamed-Mohamed-Afifi <mohamed.org050@gmail.com>
2025-01-03 04:17:10 +0200

    removing lazy load

--- src/components/video/video.component.jsx
+++ src/components/video/video.component.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import LazyLoad from "react-lazy-load";
+// import React from "react";
+// // import LazyLoad from "react-lazy-load";
 
-const Video = (props) => {
-  return (
-    <LazyLoad width="100vw" height="auto" offsetTop={200}>
-      <iframe
-        src={props.source}
-        frameborder="0"
-        title="ProjectShowcase"
-        allowfullscreen
-      ></iframe>
-    </LazyLoad>
-  );
-};
+// const Video = (props) => {
+//   return (
+//     <LazyLoad width="100vw" height="auto" offsetTop={200}>
+//       <iframe
+//         src={props.source}
+//         frameborder="0"
+//         title="ProjectShowcase"
+//         allowfullscreen
+//       ></iframe>
+//     </LazyLoad>
+//   );
+// };
 
-export default Video;
+// export default Video;
